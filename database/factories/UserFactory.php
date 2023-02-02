3896296f9b1a42b825fcab60e70449c1c591f98c
lauratita <lauratita316@gmail.com>
2023-02-02 22:23:58 +0700

    dinamis profile

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,6 @@ class UserFactory extends Factory
             'github' => $this->faker->sentence(mt_rand(2, 5)),
             'description' => $this->faker->sentence(mt_rand(5, 10)),
             'status' => '1',
-            
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'remember_token' => Str::random(10)
